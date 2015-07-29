ca922cfd620c71b21bfec194f8e7fbd86a40cb41
Nacho Diaz <idiaz.roncero@gmail.com>
2015-07-30 00:09:18 +0200

    Scrollbar al carallo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(function(){
-	$('#diapos').flickity({
+	var diapos = $('#diapos').flickity({
 		cellSelector: '.diapo',
 		setGallerySize: false,
 		imagesLoaded:true,
 		pageDots: false
 	});
-	$('.diapo-bottom-text').mCustomScrollbar({
-		theme: "dark-thin",
-		scrollbarPosition: "outside",
-		autoHideScrollbar: true
+	if (!Modernizr.touch) {
+		$('.diapo-bottom-text').mCustomScrollbar({
+			theme: "dark-thin",
+			scrollbarPosition: "outside",
+			autoHideScrollbar: true
+		});
+	}
+	$('#timeline ul').on( 'click', 'li', function() {
+	  var index = $(this).index();
+	  diapos.flickity( 'select', index );
 	});
 });
