ea191ff256278f2cd9be3c426fbaaa6f95b5340b
Nacho Diaz <idiaz.roncero@gmail.com>
2015-07-29 18:03:21 +0200

    layotu al 200% + malihu's scrollbar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ $(function(){
 	$('#diapos').flickity({
 		cellSelector: '.diapo',
 		setGallerySize: false,
-		imagesLoaded:true
+		imagesLoaded:true,
+		pageDots: false
+	});
+	$('.diapo-bottom-text').mCustomScrollbar({
+		theme: "dark-thin",
+		scrollbarPosition: "outside",
+		autoHideScrollbar: true
 	});
 });
