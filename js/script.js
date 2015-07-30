a537a72864fae7270abb7777f6e5fb26808605a6
Nacho Diaz <idiaz.roncero@gmail.com>
2015-07-30 19:24:56 +0200

    Rediseño para no usar fixed ni scroll

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
 $(function(){
-	var diapos = $('#diapos').flickity({
-		cellSelector: '.diapo',
-		setGallerySize: false,
-		imagesLoaded:true,
-		pageDots: false
+	// Consige dinamicamente la altura de la diapo-top
+	var h;
+	$(window).on('load resize', function(){
+		h = $(window).height() - 80;
+		$('.diapo-top').height(h);
+		$('#timeline').css('top', h);
 	});
-	if (!Modernizr.touch) {
-		$('.diapo-bottom-text').mCustomScrollbar({
-			theme: "dark-thin",
-			scrollbarPosition: "outside",
-			autoHideScrollbar: true
+	// if (!Modernizr.touch) {
+	// 	$('.diapo-bottom-text').mCustomScrollbar({
+	// 		theme: "dark-thin",
+	// 		scrollbarPosition: "outside",
+	// 		autoHideScrollbar: true
+	// 	});
+	// }
+	setTimeout(function(){
+		var diapos = $('#diapos').flickity({
+			cellSelector: '.diapo',
+			// setGallerySize: false,
+			imagesLoaded:true,
+			pageDots: false,
+			prevNextButtons: false,
+
 		});
-	}
-	$('#timeline ul').on( 'click', 'li', function() {
-	  var index = $(this).index();
-	  diapos.flickity( 'select', index );
-	});
+		var timeline = $('#timeline .gallery').flickity({
+			// cellSelector: 'li',
+			// setGallerySize: false,
+			pageDots: false,
+			prevNextButtons: true,
+			asNavFor: '#diapos',
+  			contain: true,
+		});
+		// $('#timeline ul').on( 'click', 'li', function() {
+		//   var index = $(this).index();
+		//   diapos.flickity( 'select', index );
+		// });
+		$('#loading').fadeOut();
+	},1000)
 });
