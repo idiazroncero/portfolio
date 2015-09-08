5851d50831b26dd19550ba9812213a2a401eb5e0
Nacho Diaz <idiaz.roncero@gmail.com>
2015-09-08 09:47:27 +0200

    Icon font y primer mockup de diapo completa

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
 $(function(){
 	// Consige dinamicamente la altura de la diapo-top
-	var h;
-	$(window).on('load resize', function(){
-		h = $(window).height() - 80;
-		$('.diapo-top').height(h);
-		$('#timeline').css('top', h);
-	});
-	// if (!Modernizr.touch) {
-	// 	$('.diapo-bottom-text').mCustomScrollbar({
-	// 		theme: "dark-thin",
-	// 		scrollbarPosition: "outside",
-	// 		autoHideScrollbar: true
-	// 	});
-	// }
-	setTimeout(function(){
-		var diapos = $('#diapos').flickity({
+	var h, diapos, timeline, timelineHtml;
+	function getDiapos() {
+		diapos = $('#diapos').flickity({
 			cellSelector: '.diapo',
 			// setGallerySize: false,
 			imagesLoaded:true,
@@ -22,7 +10,9 @@ $(function(){
 			prevNextButtons: false,
 
 		});
-		var timeline = $('#timeline .gallery').flickity({
+	}
+	function getTimeline() {
+		timeline = $('#timeline .gallery').flickity({
 			// cellSelector: 'li',
 			// setGallerySize: false,
 			pageDots: false,
@@ -30,10 +20,40 @@ $(function(){
 			asNavFor: '#diapos',
   			contain: true,
 		});
-		// $('#timeline ul').on( 'click', 'li', function() {
-		//   var index = $(this).index();
-		//   diapos.flickity( 'select', index );
-		// });
+	}
+	timelineHtml = $('#timeline').html();
+
+	$(window).on('load resize', function(){
+		h = $(window).height() - 80;
+		$('.diapo-top').height(h);
+		$('#timeline').css('top', h);
+	});
+
+	setTimeout(function(){
+		getDiapos();
+		getTimeline();
 		$('#loading').fadeOut();
 	},1000)
+
+	$('#select select').on('change', function(){
+		$('#timeline .gallery').replaceWith(timelineHtml);
+		getTimeline();
+		switch ($(this).val()) {
+			case "drupal":
+				var cells = $('.timeline-item:not(.drupal)').each(function(){
+					timeline.flickity('remove', $(this));
+				})
+				break;
+			case "wordpress":
+				var cells = $('.timeline-item:not(.wordpress)').each(function(){
+					timeline.flickity('remove', $(this));
+				})
+				break;
+			case "shopify":
+				var cells = $('.timeline-item:not(.shopify)').each(function(){
+					timeline.flickity('remove', $(this));
+				})
+				break;
+		}
+	})
 });
