cbde2f9207b0ae7f7c07bb2b8f6b61d30bf465b5
Nacho Diaz <idiaz.roncero@gmail.com>
2015-09-09 18:40:07 +0200

    Nueva icon y nuevo sistema de imagenes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+jQuery.fn.mousehold = function(timeout, f) {
+	if (timeout && typeof timeout == 'function') {
+		f = timeout;
+		timeout = 100;
+	}
+	if (f && typeof f == 'function') {
+		var timer = 0;
+		var fireStep = 0;
+		return this.each(function() {
+			jQuery(this).mousedown(function() {
+				fireStep = 1;
+				var ctr = 0;
+				var t = this;
+				timer = setInterval(function() {
+					ctr++;
+					f.call(t, ctr);
+					fireStep = 2;
+				}, timeout);
+			})
+
+			clearMousehold = function() {
+				clearInterval(timer);
+				if (fireStep == 1) f.call(this, 1);
+				fireStep = 0;
+			}
+			
+			jQuery(this).mouseout(clearMousehold);
+			jQuery(this).mouseup(clearMousehold);
+		})
+	}
+}
+
 $(function(){
 	// Consige dinamicamente la altura de la diapo-top
 	var h, diapos, timeline, timelineHtml;
@@ -54,6 +86,20 @@ $(function(){
 					timeline.flickity('remove', $(this));
 				})
 				break;
+		}
+	})
+
+	// Controles de diapos
+	$('.diapotop-controles span').mousehold(function(){
+		var img = $(this).parent().siblings('img');
+		var imgtop = parseInt(img.css('top'), 10);
+		var diapo = $(this).parents('.diapo-top');
+		var height = diapo.height();
+		var fullHeight = diapo[0].scrollHeight;
+		if (height < fullHeight) {
+			img.css({
+				top : imgtop - 25
+			})
 		}
 	})
 });
